b26be840059f9944ec5bb538ada8d7e5a7dc7987
Gosha Frolow <prokurenoo@yandex.ru>
2024-02-23 19:45:24 +0300

    Add first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+		pip install poetry && \
+		poetry install
+start:
+		poetry run python /Users/gosha/Desktop/Dev/menu/mybot/main.py
---
+++ main.py
@@ -0,0 +1,86 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.fsm.storage.memory import MemoryStorage
+from Config_reader import config
+from aiogram import F
+from aiogram.types import Message
+from aiogram.enums import ParseMode
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher()  # все данные бота, которые мы не сохраняем в БД, будут стёрты при перезапуске
+
+# Словарь для хранения идентификаторов последних отправленных сообщений ботом по чатам
+previous_messages = {}
+
+# Обработчик для удаления предыдущего сообщения бота
+async def delete_previous_bot_message(chat_id):
+    if chat_id in previous_messages and 'bot' in previous_messages[chat_id]:
+        try:
+            await bot.delete_message(chat_id=chat_id, message_id=previous_messages[chat_id]['bot'])
+        except Exception as e:
+            logging.error(f"Ошибка при удалении предыдущего сообщения бота: {e}")
+
+# Обработчик команды /start
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text='Завтрак')],
+        [types.KeyboardButton(text='Обед')],
+        [types.KeyboardButton(text='Ужин')],
+        [types.KeyboardButton(text='Перекус')]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder='Выберите нужный раздел'
+    )
+    await message.answer(
+        f'Здравствуй {message.from_user.full_name}\n\nДобро пожаловать в наш сборник рецептов 📝\nВыберите нужный вам раздел рецептов 👀',
+        reply_markup=keyboard)
+
+
+# Обработчики для разделов рецептов
+@dp.message(F.text.lower() == 'завтрак')
+async def r_breakfast(message: types.Message):
+    await delete_previous_bot_message(message.chat.id)  # Удаление предыдущего сообщения бота
+    sent_message = await message.answer("[Завтрак 🥞](https://telegra.ph/Testovyj-recept-02-19)", parse_mode=ParseMode.MARKDOWN_V2)
+    previous_messages[message.chat.id] = {'bot': sent_message.message_id}  # Сохранение идентификатора текущего сообщения бота
+
+
+@dp.message(F.text.lower() == 'обед')
+async def r_lunch(message: types.Message):
+    await delete_previous_bot_message(message.chat.id)
+    sent_message = await message.answer("[Обед](https://telegra.ph/Stranica-s-receptami-na-obed-02-22)", parse_mode=ParseMode.MARKDOWN_V2)
+    previous_messages[message.chat.id] = {'bot': sent_message.message_id}
+
+
+@dp.message(F.text.lower() == 'ужин')
+async def r_dinner(message: types.Message):
+    await delete_previous_bot_message(message.chat.id)
+    sent_message = await message.answer("[Ужин](https://telegra.ph/Uzhin-02-22)", parse_mode=ParseMode.MARKDOWN_V2)
+    previous_messages[message.chat.id] = {'bot': sent_message.message_id}
+
+@dp.message(F.text.lower() == 'перекус')
+async def r_snack(message: types.Message):
+    await delete_previous_bot_message(message.chat.id)
+    sent_message = await message.answer("[Перекус](https://telegra.ph/Perekus-02-22)", parse_mode=ParseMode.MARKDOWN_V2)
+    previous_messages[message.chat.id] = {'bot': sent_message.message_id}
+
+
+# Обработчик команды /offer
+@dp.message(Command('offer'))
+async def cmd_offer(message: types.Message):
+    await delete_previous_bot_message(message.chat.id)
+    sent_message = await message.answer('Для предложения своего рецепта напишите *@Glymm*',
+                                       parse_mode=ParseMode.MARKDOWN_V2)
+    previous_messages[message.chat.id] = {'bot': sent_message.message_id}
+
+# Основная функция
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
